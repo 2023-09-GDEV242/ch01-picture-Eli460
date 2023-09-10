937414cd05b42f7dd058011b7e89981b260db8b5
ElishaWhite <123849710+Eli460@users.noreply.github.com>
2023-09-10 13:39:00 -0400

    made a basketball scene

--- Picture.java
+++ Picture.java
@@ -11,21 +11,33 @@
  */
 public class Picture
 {
-    private Square wall;
+    private Square court;
     private Square window;
-    private Triangle roof;
+    private Triangle rim;
     private Circle sun;
     private boolean drawn;
+    private Square line1;
+    private Person player;
+    private Person player2;
+    private Square hoop1;
+    private Square hoop2;
+    private Circle basketball;
 
     /**
      * Constructor for objects of class Picture
      */
     public Picture()
     {
-        wall = new Square();
-        window = new Square();
-        roof = new Triangle();  
-        sun = new Circle();
+        court = new Square();
+        line1= new Square();
+        hoop1 = new Square();
+        hoop2 = new Square();
+        //window = new Square();
+        rim = new Triangle();  
+        //sun = new Circle();
+        player = new Person();
+        player2 = new Person();
+        basketball = new Circle();
         drawn = false;
     }
 
@@ -35,27 +47,66 @@ public class Picture
     public void draw()
     {
         if(!drawn) {
-            wall.moveHorizontal(-140);
-            wall.moveVertical(20);
-            wall.changeSize(120);
-            wall.makeVisible();
+            court.moveHorizontal(-320);
+            court.moveVertical(-320);
+            court.changeSize(550);
+            court.makeVisible();
+            court.changeColor("blue");
             
-            window.changeColor("black");
-            window.moveHorizontal(-120);
-            window.moveVertical(40);
-            window.changeSize(40);
-            window.makeVisible();
+            line1.changeColor("green");
+            line1.moveHorizontal(-360);
+            line1.moveVertical(100);
+            line1.changeSize(550);
+            line1.makeVisible();
+        
+            player.changeSize(80,40);
+            player.moveHorizontal(100);
+            player.moveVertical(25);
+            player.makeVisible();
+        
+            player2.changeSize(80,40);
+            player2.moveHorizontal(-50);
+            player2.moveVertical(25);
+            player2.makeVisible();
+            
     
-            roof.changeSize(60, 180);
-            roof.moveHorizontal(20);
-            roof.moveVertical(-60);
-            roof.makeVisible();
+            hoop1.changeColor("white");
+            hoop1.moveHorizontal(-250);
+            hoop1.moveVertical(-55);
+            hoop1.changeSize(80);
+            hoop1.makeVisible();
+        
+            hoop2.changeColor("black");
+            hoop2.moveHorizontal(-230);
+            hoop2.moveVertical(-35);
+            hoop2.changeSize(35);
+            hoop2.makeVisible();
+            
+            rim.changeColor("orange");
+            rim.changeSize(20, 30);
+            rim.moveHorizontal(-65);
+            rim.moveVertical(-15);
+            rim.makeVisible();
+        
+            basketball.changeColor("orange");
+            basketball.moveHorizontal(15);
+            basketball.moveVertical(-10);
+            basketball.changeSize(18);
+            basketball.makeVisible();
+            
+            //window.changeColor("black");
+            //window.moveHorizontal(-120);
+            //window.moveVertical(40);
+            //window.changeSize(40);
+            //window.makeVisible();
     
-            sun.changeColor("yellow");
-            sun.moveHorizontal(100);
-            sun.moveVertical(-40);
-            sun.changeSize(80);
-            sun.makeVisible();
+
+    
+            //sun.changeColor("yellow");
+            //sun.moveHorizontal(100);
+            //sun.moveVertical(-40);
+            //sun.changeSize(80);
+            //sun.makeVisible();
             drawn = true;
         }
     }
@@ -65,10 +116,11 @@ public class Picture
      */
     public void setBlackAndWhite()
     {
-        wall.changeColor("black");
-        window.changeColor("white");
-        roof.changeColor("black");
-        sun.changeColor("black");
+        court.changeColor("black");
+         line1.changeColor("black");
+        //window.changeColor("white");
+        //roof.changeColor("black");
+        //sun.changeColor("black");
     }
 
     /**
@@ -76,9 +128,10 @@ public class Picture
      */
     public void setColor()
     {
-        wall.changeColor("red");
-        window.changeColor("black");
-        roof.changeColor("green");
-        sun.changeColor("yellow");
+        court.changeColor("green");
+        line1.changeColor("black");
+        //window.changeColor("black");
+        //roof.changeColor("green");
+        //sun.changeColor("yellow");
     }
 }
